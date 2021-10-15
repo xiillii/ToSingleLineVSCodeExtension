b1290509cff9913d232be4cd4c8dccab7d85feb6
Jose Alonso <jarommat@gmail.com>
2021-10-14 22:59:18 -0500

    Fix BUG: Car returns on Windows  Systems and replace 2 or more whitespaces with one space

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,8 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tosingleline" is now active!');
+
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,21 +14,24 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('tosingleline.execute', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		
+
 
 		let editor = vscode.window.activeTextEditor;
-		let document = editor?.document;  
+		let document = editor?.document;
 		if (document) {
 			let text = document.getText();
 
-			const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(text.length - 1));
+			const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(text.length));
 
-			const searchRegExp = /\n/g;
+			const searchRegExpReturns = /[\r\n]/gm;
 			const replaceWith = ' ';
+			const searchRegExpTwoOrMoreSpaces = /\s{2,}/gm;
 
-			text = text.replace(searchRegExp, replaceWith);
+			text = text.replace(searchRegExpReturns, replaceWith);
+			text = text.replace(searchRegExpTwoOrMoreSpaces, replaceWith);
 			editor?.edit(e => e.replace(fullRange, text));
 			vscode.window.showInformationMessage('Text processed with ToSingleLine!');
+
 		}
 		else {
 			vscode.window.showErrorMessage('A text editor must be active');
@@ -41,4 +42,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
